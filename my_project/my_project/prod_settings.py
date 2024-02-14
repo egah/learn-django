f8f443419fe6c0bd286f91c8acb8c5486d60eb04
Epiphane EGAH <egahepiphane@gmail.com>
2024-02-14 13:46:17 +0100

    update

--- my_project/my_project/prod_settings.py
+++ my_project/my_project/prod_settings.py
@@ -3,16 +3,15 @@ from my_project.settings import *
 
 import django_on_heroku
 
-'''
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 MIDDLEWARE += ("whitenoise.middleware.WhiteNoiseMiddleware",)
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
-'''
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
 
 django_on_heroku.settings(locals())
 
